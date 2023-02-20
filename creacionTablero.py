8c3f830b190ae4aa00421609a6a21ce90cae4e68
SmillerMP <samuelmp7@gmail.com>
2023-02-20 11:32:11 -0600

    Arreglos al tablero y alineacion
    se corrigieron algunos problemas en la alineacion horizontal del tablero

--- creacionTablero.py
+++ creacionTablero.py
@@ -6,23 +6,32 @@ def tablero(columnas, filas):
     tablero_dot = open("tablero.dot", "w")
     tablero_dot.write('graph { \n')
     tablero_dot.write('rankdir = TB \n' )
-    tablero_dot.write('node[shape=record, fontname="Arial", fontsize=15] \n')
+    tablero_dot.write('node[shape= rect, fontname = "Arial", fontsize = 14] \n')
 
-
+    # Crea las conexiones de manera verticasl
     for x in range(columnas):
         
         for y in range(filas-1):
-            tablero_dot.write("celda" + str(y) + str(x) + " -- " + "celda" + str(y+1) + str(x) + "\n")
+            tablero_dot.write("F" + str(y) + "C" + str(x) + " -- " + "F" + str(y+1) + "C" + str(x) + "\n")
     
         tablero_dot.write("\n")
 
 
+    # Crea las conexiones de manera horizontal
+    for y in range(filas):
+        for x in range(columnas-1):
+            tablero_dot.write("F" + str(y) + "C" + str(x) + " -- " + "F" + str(y) + "C" + str(x+1) + "\n")
+        
+        tablero_dot.write("\n")
 
+    tablero_dot.write("\n\n")
+
+    # Encuadre de los nodos
     tablero_dot.write("/*------------ Encuadre ------------*/\n")
     for y in range(filas):
         tablero_dot.write("{rank = same")
         for x in range(columnas):
-            tablero_dot.write("; celda" + str(y) + str(x))
+            tablero_dot.write("; F" + str(y) + "C" + str(x))
 
         tablero_dot.write("} \n")
     
